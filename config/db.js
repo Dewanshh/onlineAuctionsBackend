1c4b90d30590db340b7ab5c95fe32f8895edd914
Dewansh Jangir <dewanshjangir@Dewanshs-MacBook-Air-2.local>
2024-12-19 10:29:31 +0530

    New Updates

--- config/db.js
+++ config/db.js
@@ -1,4 +1,3 @@
-// db.js
 const { Sequelize } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -3,10 +3,7 @@ const {loginUser,registerUser, registerCustomer} = require('../controllers/authC
 
 const router = express.Router();
 
-// Route for user registration
-// router.post('/register', register);
 
-// Route for user login
 router.post('/login', loginUser);
 router.post("/register", registerUser);
 router.post("/registerCustomer",registerCustomer);
