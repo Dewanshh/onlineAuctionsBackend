b014f2beb39e192e398d99e72255c5649130d7b0
Dewansh Jangir <dewanshjangir@Dewanshs-MacBook-Air-2.local>
2024-12-27 19:46:54 +0530

    New Updates

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,7 +1,9 @@
 
 
-const { where } = require("sequelize");
+const { where,Op } = require("sequelize");
 const {Product} = require("../models/Products");
+const {Customer} = require("../models/Customer");
+
 
   
 exports.createProduct=async(req, res)=> {
@@ -39,6 +41,40 @@ exports.getProductById=async (req,res)=>{
     const id=req.params.id;
     const response=await Product.findOne({where:{id:id}});
     res.status(200).json(response)
+  }catch(e)
+  {
+    res.status(500).json({"Error":e});
+    console.log(e);
+  }
+}
+
+exports.deleteProductById=async (req,res)=>{
+  try{
+    const id=req.params.id;
+    
+    const customers = await Customer.findAll({
+      where: {
+        customerBids: {
+          [Op.like]: `%${id}%`,
+        }
+      }
+    });
+    
+    for (let customer of customers) {
+      let customerBids = JSON.parse(customer.customerBids) || [];
+      
+      
+      const updatedBids = customerBids.filter(bid => bid !== id.toString());
+      
+      customer.customerBids = JSON.stringify(updatedBids);
+      await customer.save();
+    }
+    
+    const response=await Product.destroy({where:{id:id}});
+
+    if(!response) res.status(404).json({"Error":"Product Not Found"});
+
+    res.status(200).json("Product Deleted Succesfully.")
   }catch(e)
   {
     res.status(500).json({"Error":e});
--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,5 +1,5 @@
 
-const { createProduct, getProducts, getProductById } = require("../controllers/productController");
+const { createProduct, getProducts, getProductById, deleteProductById } = require("../controllers/productController");
 const express = require('express');
 const router = express.Router();
 
@@ -7,5 +7,6 @@ const router = express.Router();
 router.post('/create', createProduct);
 router.get('/all',getProducts);
 router.get('/:id',getProductById);
+router.delete('/:id',deleteProductById);
 
 module.exports = router;
